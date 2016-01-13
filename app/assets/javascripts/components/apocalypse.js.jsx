e471bf3a6587399256426fd1f55b46e805b560b1
Tony Goncalves <tony@redlightsoft.com>
2016-01-13 15:08:55 +0000

    Component with static data and some minor CSS

--- app/assets/javascripts/components/apocalypse.js.jsx
+++ app/assets/javascripts/components/apocalypse.js.jsx
@@ -1,8 +1,65 @@
 const Apocalypse = React.createClass({
 
+  getInitialState() {
+    return {
+      counting: false,
+      count: 0,
+      errors: ['there was an error due to some very extremely wacky stuff that just happened a while ago man, wtf?!=!?! stuff...', 'there was another error']
+    };
+  },
+
+
+  renderCount() {
+    return <span>{this.state.count}</span>;
+  },
+
+
+  renderStartStop() {
+    if (this.state.counting) {
+      return <button type='button' className='btn btn-warning ml-10'>Stop</button>;
+    }
+    else {
+      return <button type='button' className='btn btn-success ml-10'>Start</button>;
+    }
+  },
+
+
+  renderReset() {
+    return <button type='button' className='btn btn-danger ml-10'>Reset</button>;
+  },
+
+
+  renderErrors() {
+    if (this.state.errors.length > 0) {
+      return (
+        <div className='alert alert-danger mt-100 w-40pct' role='alert'>
+        {this.state.errors.map((e) =>
+          <div className='mt-5 mb-5'>
+          <span className='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>
+          <span className='sr-only'>Error:</span> {e}
+        </div>
+        )}
+        </div>
+      );
+    }
+  },
+
+
   render() {
     return(
-      <h1>Well hi there, great dark overlord!</h1>
+      <div className='ml-100'>
+        <h1>Well hi there, great dark overlord!</h1>
+
+        <div className='mt-100 w-40pct timer-box'>
+          <div className='pull-right'>
+            {this.renderCount()}
+            {this.renderStartStop()}
+            {this.renderReset()}
+          </div>
+        </div>
+
+        {this.renderErrors()}
+      </div>
     );
   }
 
